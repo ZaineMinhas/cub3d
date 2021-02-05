24f0e98a5cd2aefbda52ba46c10d659868577ca4
ZaineMinhas <zaineminhas5@gmail.com>
2021-02-05 18:41:18 +0100

    presque cub2d

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 14:53:01 by zminhas           #+#    #+#             */
-/*   Updated: 2021/02/03 17:01:16 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/02/05 18:38:02 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 # define CUB3D_H
 
 # include <mlx.h>
+# include <stdlib.h>
 
-typedef struct	s_cub3dlist
+int g_rose = 32123123;
+int g_orange = 100100100;
+
+typedef struct	s_cublist
 {
 	int	p_x;
 	int	p_y;
-	int	weight;
-	void	*ptr;
-	void	*ptr2;
-}				t_cub3dlist;
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_cublist;
 
 #endif
---
+++ main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "cub3d.h"
+
+void ft_reset(void *mlx_ptr, void *win_ptr)
+{
+	int x;
+	int y;
+
+	x = -1;
+	y = -1;
+	while (++x < 1921)
+	{
+		y = -1;
+		while (++y < 1081)
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y, 255255255);
+	}
+}
+
+void ft_square(t_cublist *var)
+{
+	int tmp_x;
+	int tmp_y;
+	int y_init;
+
+	tmp_x = var->p_x + 25;
+	tmp_y = var->p_y + 25;
+	y_init = var->p_y;
+	while (var->p_x++ < tmp_x)
+	{
+		var->p_y = y_init;
+		while (var->p_y++ < tmp_y)
+			mlx_pixel_put(var->mlx_ptr, var->win_ptr, var->p_x, var->p_y, 050050050);
+	}
+}
+
+void ft_go_up(t_cublist *var)
+{
+	var->p_y -= 5;
+	ft_square(var);
+}
+
+void ft_go_left(t_cublist *var)
+{
+	var->p_x -= 5;
+	ft_square(var);
+}
+
+void ft_go_down(t_cublist *var)
+{
+	var->p_y += 5;
+	ft_square(var);
+}
+
+void ft_go_right(t_cublist *var)
+{
+	var->p_x += 5;
+	ft_square(var);
+}
+
+int ft_go(int key, t_cublist *var)
+{
+	if (key == 53)
+		exit(1);
+	else if (key == 13 /*w*/)
+		ft_go_up(var);
+	else if (key == 0 /*a*/)
+		ft_go_left(var);
+	else if (key == 1 /*s*/)
+		ft_go_down(var);
+	else if (key == 2 /*d*/)
+		ft_go_right(var);
+	// else if (key == 123 /*left*/)
+	// 	ft_look_left(var);
+	// else if (key == 124 /*right*/)
+	// 	ft_look_right(var);
+	return (0);
+}
+
+int main(void)
+{
+	t_cublist var;
+
+	var.p_x = 1920 / 2;
+	var.p_y = 1080 / 2;
+	var.mlx_ptr = mlx_init();
+	var.win_ptr = mlx_new_window(var.mlx_ptr, 1920, 1080, "la fenetre");
+	//ft_reset(var.mlx_ptr, var.win_ptr);
+	ft_square(&var);
+	mlx_key_hook(var.win_ptr, ft_go, &var);
+	mlx_loop(var.mlx_ptr);
+	return (0);
+}
+
+// w = 13, a = 0, s = 1, d = 2, up = 126, left = 123, right = 124, down = 125
+
+// la commande : gcc -lmlx -framework OpenGL -framework AppKit main.c && clear && ./a.out 
