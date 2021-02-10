c2e9e92fcdec3ef04665388485bc7309b4546e5a
ZaineMinhas <zaineminhas5@gmail.com>
2021-02-10 19:43:02 +0100

    ca avance pas mal

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 14:53:01 by zminhas           #+#    #+#             */
-/*   Updated: 2021/02/05 18:38:02 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/02/10 19:41:00 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 # define CUB3D_H
 
 # include <mlx.h>
+# include <math.h>
 # include <stdlib.h>
 
 int g_rose = 32123123;
 int g_orange = 100100100;
+int g_red = 150150150;
 
 typedef struct	s_cublist
 {
-	int	p_x;
-	int	p_y;
+	float	p_x;
+	float	p_y;
+	int rot;
 	void	*mlx_ptr;
 	void	*win_ptr;
 }				t_cublist;
--- main.c
+++ main.c
@@ -16,60 +16,207 @@ void ft_reset(void *mlx_ptr, void *win_ptr)
 	}
 }
 
-void ft_square(t_cublist *var)
+void ft_wall(t_cublist var, int wall, int x, int y)
 {
+	int color;
 	int i;
 	int j;
 
+	if (wall)
+		color = g_orange;
+	else
+		color = 0;
 	i = -1;
-	while (++i < 10)
+	while (++i < 100)
 	{
 		j = -1;
-		while (++j < 10)
-			mlx_pixel_put(var->mlx_ptr, var->win_ptr, var->p_x + i, var->p_y + j, g_rose);
+		while (++j < 107)
+			mlx_pixel_put(var.mlx_ptr, var.win_ptr, x * 101 + i, y * 108 + j, color);
 	}
 }
 
-void ft_go_up(t_cublist *var)
+void ft_map(t_cublist var)
 {
-	var->p_y -= 5;
-	ft_square(var);
+	int i;
+	int j;
+	int map_x;
+	int map_y;
+	int map[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+				1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 
+				1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+				1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+				1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+				1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+				1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+				1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+				1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+				1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	map_x = 19;
+	map_y = 10;
+	i = -1;
+	while (++i < map_x)
+	{
+		j = -1;
+		while (++j < map_y)
+			ft_wall(var, map[j * map_x + i], i, j);
+	}
+
+
 }
 
-void ft_go_left(t_cublist *var)
+void ft_square(t_cublist *var, int color)
 {
-	var->p_x -= 5;
-	ft_square(var);
+	float dx;
+	float dy;
+	float x;
+	float y;
+	int i;
+
+	dx = cos(var->rot * (M_PI / 180));
+	dy = sin(var->rot * (M_PI / 180));
+	i = -1;
+	while (++i < 15)
+	{
+		x = -1;
+		while (++x < 15)
+			mlx_pixel_put(var->mlx_ptr, var->win_ptr, var->p_x + i, var->p_y + x, color);
+	}
+	i = -1;
+	x = 0;
+	y = 0;
+	if (color)
+		color = g_red;
+	while (++i < 40)
+	{
+		x += dx;
+		y += dy;
+		mlx_pixel_put(var->mlx_ptr, var->win_ptr, x + var->p_x, var->p_y - y, color);
+	}
 }
 
-void ft_go_down(t_cublist *var)
+void ft_go_w(t_cublist *var)
 {
-	var->p_y += 5;
-	ft_square(var);
+	float dx;
+	float dy;
+
+	ft_square(var, 0);
+	dx = cos(var->rot);
+	dy = sin(var->rot);
+	var->p_x -= dx;
+	var->p_y -= dy;
+	ft_square(var, g_rose);
 }
 
-void ft_go_right(t_cublist *var)
+void ft_go_a(t_cublist *var)
 {
-	var->p_x += 5;
-	ft_square(var);
+	float dx;
+	float dy;
+
+	ft_square(var, 0);
+	dx = cos((M_PI / 180) * var->rot * 10);
+	dy = sin((M_PI / 180) * var->rot * 10);
+	var->p_x -= dy;
+	var->p_x += dx;
+	ft_square(var, g_rose);
+}
+
+void ft_go_s(t_cublist *var)
+{
+	float dx;
+	float dy;
+	ft_square(var, 0);
+	var->p_x += dx;
+	var->p_y += dy;
+	ft_square(var, g_rose);
+}
+
+void ft_go_d(t_cublist *var)
+{
+	float dx;
+	float dy;
+
+	ft_square(var, 0);
+	dx = cos((M_PI / 180) * var->rot * 10);
+	dy = sin((M_PI / 180) * var->rot * 10);
+	var->p_x += dy;
+	var->p_y -= dx;
+	ft_square(var, g_rose);
+}
+
+void ft_look_right(t_cublist *var)
+{
+	ft_square(var, 0);
+	var->rot -= 15;
+	var->rot %= 360;
+	ft_square(var, g_rose);
+}
+
+void ft_look_left(t_cublist *var)
+{
+	ft_square(var, 0);
+	var->rot += 15;
+	var->rot %= 360;
+	ft_square(var, g_rose);
+}
+
+void ft_go_rot(t_cublist *var, int key)
+{
+	if (!var->rot)
+	{
+		if (key == 13 /*w*/)
+			ft_go_up(var);
+		else if (key == 0 /*a*/)
+			ft_go_left(var);
+		else if (key == 1 /*s*/)
+			ft_go_down(var);
+		else if (key == 2 /*d*/)
+			ft_go_right(var);
+	}
+	else if (var->rot == 1)
+	{
+		if (key == 13 /*w*/)
+			ft_go_right(var);
+		else if (key == 0 /*a*/)
+			ft_go_up(var);
+		else if (key == 1 /*s*/)
+			ft_go_left(var);
+		else if (key == 2 /*d*/)
+			ft_go_down(var);
+	}
+	else if (var->rot == 2)
+	{
+		if (key == 13 /*w*/)
+			ft_go_down(var);
+		else if (key == 0 /*a*/)
+			ft_go_right(var);
+		else if (key == 1 /*s*/)
+			ft_go_up(var);
+		else if (key == 2 /*d*/)
+			ft_go_left(var);
+	}
+	else if (var->rot == 3)
+	{
+		if (key == 13 /*w*/)
+			ft_go_left(var);
+		else if (key == 0 /*a*/)
+			ft_go_down(var);
+		else if (key == 1 /*s*/)
+			ft_go_right(var);
+		else if (key == 2 /*d*/)
+			ft_go_up(var);
+	}
 }
 
 int ft_go(int key, t_cublist *var)
 {
-	if (key == 53)
+	if (key == 53 /*esc*/)
 		exit(1);
-	else if (key == 13 /*w*/)
-		ft_go_up(var);
-	else if (key == 0 /*a*/)
-		ft_go_left(var);
-	else if (key == 1 /*s*/)
-		ft_go_down(var);
-	else if (key == 2 /*d*/)
-		ft_go_right(var);
-	// else if (key == 123 /*left*/)
-	// 	ft_look_left(var);
-	// else if (key == 124 /*right*/)
-	// 	ft_look_right(var);
+	else if (key == 123 /*left*/)
+		ft_look_left(var);
+	else if (key == 124 /*right*/)
+		ft_look_right(var);
+	if (key == 13 || key == 0 || key == 1 || key == 2)
+		ft_go_rot(var, key);
 	return (0);
 }
 
@@ -79,10 +226,12 @@ int main(void)
 
 	var.p_x = 1920 / 2;
 	var.p_y = 1080 / 2;
+	var.rot = 90;
 	var.mlx_ptr = mlx_init();
 	var.win_ptr = mlx_new_window(var.mlx_ptr, 1920, 1080, "la fenetre");
 	//ft_reset(var.mlx_ptr, var.win_ptr);
-	ft_square(&var);
+	ft_map(var);
+	ft_square(&var, g_rose);
 	mlx_key_hook(var.win_ptr, ft_go, &var);
 	mlx_loop(var.mlx_ptr);
 	return (0);
@@ -90,4 +239,4 @@ int main(void)
 
 // w = 13, a = 0, s = 1, d = 2, up = 126, left = 123, right = 124, down = 125
 
-// la commande : gcc -lmlx -framework OpenGL -framework AppKit main.c && clear && ./a.out 
+// la commande : gcc -lmlx -framework OpenGL -framework AppKit main.c && clear && ./a.out
